f5570064755c72e1991f7f512376df49f79f5284
0svaldojunior <app.o.r.jr@gmail.com>
2023-05-12 13:05:47 -0300

    update storybook version

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,31 +1,22 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
-    "storybook-dark-mode",
-  ],
-  core: {
-    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
-  },
+  stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", '@storybook/addon-a11y', "storybook-dark-mode", "@storybook/addon-mdx-gfm"],
+  core: {},
   framework: {
     name: "@storybook/react-webpack5",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
+    autodocs: "tag"
   },
-  viteFinal: async (config, { configType }) => {
+  viteFinal: async (config, {
+    configType
+  }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/BuyeeMeDesignSystem/'
+      config.base = '/BuyeeMeDesignSystem/';
     }
     return config;
-  },
+  }
 };
 export default config;
